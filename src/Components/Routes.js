ac4471ca06a757ea1679470c68a1d71290eb4aab
asim samuel <asim.samuel1@gmail.com>
2020-10-28 16:57:25 -0400

    Chore: created routes components

---
+++ src/Components/Routes.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import Quiz from './Quiz'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <main>
+      <h1>
+        Welcome To Tandem Trivia!
+      </h1>
+    </main>
+  )
+}
+
+const Routes = () => {
+
+  return (
+    <Router>
+      <div id="top-level-page">
+        <Navbar />
+        <div className="main-content">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/practice" component={Quiz} />
+          </Switch>
+
+        </div>
+      </div>
+    </Router>
+
+  )
+
+
+}
+
+export default Routes
